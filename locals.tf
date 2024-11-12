44c9cb322fa24d4d6ef3d0ad630245222ae33e08
Hangyu Xu <hangxu@microsoft.com>
2024-11-12 21:51:42 +0800

    omit null value

--- locals.tf
+++ locals.tf
@@ -11,4 +11,55 @@ locals {
   }
   dynamic_memory_config_omit_null    = { for k, v in local.dynamic_memory_config_full : k => v if v != null }
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
+  virtual_machine_properties = {
+    hardwareProfile = {
+      vmSize              = "Custom"
+      processors          = var.v_cpu_count
+      memoryMB            = var.memory_mb
+      dynamicMemoryConfig = length(keys(local.dynamic_memory_config_omit_null)) == 0 ? null : local.dynamic_memory_config_omit_null
+    }
+    httpProxyConfig = var.http_proxy == null && var.https_proxy == null ? null : {
+      httpProxy  = var.http_proxy
+      httpsProxy = var.https_proxy
+      noProxy    = var.no_proxy
+      trustedCa  = var.trusted_ca
+    }
+    osProfile = {
+      adminUsername = var.admin_username
+      adminPassword = var.admin_password
+      computerName  = var.name
+      linuxConfiguration = {
+        ssh = var.linux_ssh_config == null ? {} : var.linux_ssh_config
+      }
+      windowsConfiguration = {
+        provisionVMAgent       = true
+        provisionVMConfigAgent = true
+        ssh                    = var.windows_ssh_config == null ? {} : var.windows_ssh_config
+      }
+    }
+    securityProfile = {
+      uefiSettings = {
+        secureBootEnabled = var.secure_boot_enabled
+      }
+    }
+    storageProfile = {
+      vmConfigStoragePathId = var.user_storage_id == "" ? null : var.user_storage_id
+      imageReference = {
+        id = var.image_id
+      }
+      dataDisks = [for i in range(length(var.data_disk_params)) : {
+        id = azapi_resource.data_disks[i].id
+      }]
+      osDisk = {
+        osType = var.os_type
+      }
+    }
+    networkProfile = {
+      networkInterfaces = [
+        {
+          id = azapi_resource.nic.id
+        }
+      ]
+    }
+  }
 }
--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ resource "azapi_resource" "hybrid_compute_machine" {
   type = "Microsoft.HybridCompute/machines@2023-10-03-preview"
   body = {
     kind = "HCI",
+    properties = null,
   }
   location  = var.location
   name      = var.name
@@ -53,57 +54,7 @@ resource "azapi_resource" "virtual_machine" {
       type = "CustomLocation"
       name = var.custom_location_id
     }
-    properties = merge({
-      hardwareProfile = {
-        vmSize              = "Custom"
-        processors          = var.v_cpu_count
-        memoryMB            = var.memory_mb
-        dynamicMemoryConfig = length(keys(local.dynamic_memory_config_omit_null)) == 0 ? null : local.dynamic_memory_config_omit_null
-      }
-      httpProxyConfig = var.http_proxy == null && var.https_proxy == null ? null : {
-        httpProxy  = var.http_proxy
-        httpsProxy = var.https_proxy
-        noProxy    = var.no_proxy
-        trustedCa  = var.trusted_ca
-      }
-      osProfile = {
-        adminUsername = var.admin_username
-        adminPassword = var.admin_password
-        computerName  = var.name
-        linuxConfiguration = {
-          ssh = var.linux_ssh_config == null ? {} : var.linux_ssh_config
-        }
-        windowsConfiguration = {
-          provisionVMAgent       = true
-          provisionVMConfigAgent = true
-          ssh = var.windows_ssh_config == null ? {} : var.windows_ssh_config
-        }
-      }
-      securityProfile = {
-        uefiSettings ={
-          secureBootEnabled = var.secure_boot_enabled
-        }
-      }
-      storageProfile = {
-        vmConfigStoragePathId = var.user_storage_id == "" ? null : var.user_storage_id
-        imageReference = {
-          id = var.image_id
-        }
-        dataDisks = [for i in range(length(var.data_disk_params)) : {
-          id = azapi_resource.data_disks[i].id
-        }]
-        osDisk = {
-          osType = var.os_type
-        }
-      }
-      networkProfile = {
-        networkInterfaces = [
-          {
-            id = azapi_resource.nic.id
-          }
-        ]
-      }
-    })
+    properties = { for key, value in local.virtual_machine_properties : key => value if value != null }
   }
   name      = "default" # value must be 'default' per 2023-09-01-preview
   parent_id = azapi_resource.hybrid_compute_machine.id
